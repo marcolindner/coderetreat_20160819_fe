48410ba4cd76c00b6b77e2cc39a03c471794d33a
Marco Lindner <marco.lindner@gmx.org>
2016-09-06 11:20:09 +0200

    Adding some tests

---
+++ js/potter.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var Basket = function () {
+    this.discounts = [0, 1, 0.95, 0.9, 0.8, 0.75];
+    this.bookPrice = 8;
+};
+
+Basket.prototype.buildPackages = function (books, maxCount) {
+    var count = 0;
+    for (var i = 0; i < books.length; i++) {
+        if (count === maxCount) {
+            break;
+        }
+
+        if (books[i] > 0) {
+            books[i]--;
+            count++;
+        }
+    }
+    return count;
+};
+
+Basket.prototype.calculatePriceForPackages = function (books, maxCount) {
+    var totalPrice = 0, count = 0;
+
+    do {
+        count = this.buildPackages(books, maxCount);
+        totalPrice += count * this.bookPrice * this.discounts[count];
+    } while (count > 0);
+
+    return totalPrice;
+};
+
+
+Basket.prototype.calculate = function (books) {
+    books = books || [];
+    if (books.length === 0) {
+        return 0;
+    }
+
+    var bestPrice = 0;
+
+    for (var maxCount = 5; maxCount > 0; maxCount--) {
+        var price = this.calculatePriceForPackages(books.slice(0), maxCount);
+        if (price > 0 && (bestPrice === 0 || bestPrice > price)) {
+            bestPrice = price;
+        } else {
+            break;
+        }
+    }
+
+    return bestPrice;
+};
---
+++ js/potter.spec.js
@@ -0,0 +1,56 @@
+/* global Basket */
+'use strict';
+
+describe('potterSpec', function () {
+    var basket = new Basket();
+    describe('When Basket is empty', function () {
+        it('Should return 0', function () {
+            expect(basket.calculate()).toBe(0);
+        });
+    });
+    describe('When Basket is empty', function () {
+        it('Should return 0', function () {
+            expect(basket.calculate([])).toBe(0);
+        });
+    });
+    describe('When Basket contains just one book', function () {
+        it('Should calculate the full price for the book', function () {
+            expect(basket.calculate([1])).toBe(8);
+        });
+    });
+    describe('When Basket contains the same book twice', function () {
+        it('Should calculate with the full price for both books', function () {
+            expect(basket.calculate([2])).toBe(16);
+        });
+    });
+    describe('When Basket contains the same book three times', function () {
+        it('Should calculate with the full price for three books', function () {
+            expect(basket.calculate([3])).toBe(24);
+        });
+    });
+    describe('When Basket contains the same book four times', function () {
+        it('Should calculate with the full price for four books', function () {
+            expect(basket.calculate([4])).toBe(32);
+        });
+    });
+    describe('When Basket contains the same book five times', function () {
+        it('Should calculate with the full price for five books', function () {
+            expect(basket.calculate([5])).toBe(41);
+        });
+    });
+    describe('When Basket contains two different books', function () {
+        it('Should give 5 percent discount for each book', function () {
+            expect(basket.calculate([1, 1])).toBe(15.2);
+        });
+    });
+    describe('When Basket contains three different books', function () {
+        it('Should give 5 percent discount for each book', function () {
+            expect(basket.calculate([1, 1, 1])).toBe(22.8);
+        });
+    });
+    describe('When Basket contains more books', function () {
+        it('Should give the user the best price', function () {
+            expect(basket.calculate([2, 2, 2, 1, 1])).toBe(51.2);
+        });
+    });
+});
